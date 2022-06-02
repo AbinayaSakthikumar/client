b3359b9080f9d622fd593788f786772d28167703
AbinayaSakthikumar <abinayaabi12396@gmail.com>
2022-06-02 16:39:52 +0530

    End of section 10( 123. Using the service to store state)

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -38,10 +38,11 @@ export class MemberEditComponent implements OnInit {
   }
 
     updateMember() {
-      console.log(this.member);
+      this.memberService.updateMember(this.member).subscribe(() => {
+
+    
       this.toastr.success(' Profile updated successfully');
       this.editForm.reset(this.member);
+    })
     }
-
-
 }
